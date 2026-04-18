a39cf1ffa9c2fa7474bb7452e430315065a4ba05
Rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2026-04-18 21:28:42 +0600

    [go-vercel] (refactor/main): Clean up import formatting and spacing in main.go

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
- 
+
 import (
-  "fmt"
-  "log"
-  "net/http"
-  "os"
+	"fmt"
+	handler "go-vercel/api"
+	"log"
+	"net/http"
+	"os"
 )
  
 func main() {
@@ -12,11 +13,12 @@ func main() {
   mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintln(w, "Hello from Go on Vercel")
   })
- 
+  mux.HandleFunc("/static", handler.Handler)
+
   port := os.Getenv("PORT")
   if port == "" {
     port = "3000"
   }
- 
+
   log.Fatal(http.ListenAndServe(":"+port, mux))
 }
